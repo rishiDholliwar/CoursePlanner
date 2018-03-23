5508b84be0d6790669e895aa2933675b4cc969d4
rishi <rdholliw@sfu.ca>
2018-03-23 08:00:04 -0700

    init commit

---
+++ src/main/java/ca/cmpt213/as5/CoursePlanner/Application.java
@@ -0,0 +1,14 @@
+package ca.cmpt213.as5.CoursePlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * start of application for Course Planner
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
